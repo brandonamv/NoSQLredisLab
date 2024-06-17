24cd60aba8af9e3648575727648d2f44d3519e48
brandonamv <46854298+brandonamv@users.noreply.github.com>
2024-06-17 01:01:08 -0400

    update

--- lab1.py
+++ lab1.py
@@ -1,26 +1,41 @@
 # Brandon Martinez
 # Erika Rosales
 import redis
+
+#conectando a la base de datos local
 r=redis.StrictRedis(host='localhost',port=6379,db=0)
 
 #r.flushall()
+
+#Lectura de archivo
 file1 = open("entrada.in", "r") 
+#separando por lineas
 lines=file1.read().split('\n')
 for l in lines:
-    data=l.split('\t')
-    if(data.__len__()>1):
+    #separando por tab
+    data=l.split('\t')  
+    #guardando solo las claves validas 
+    if(data.__len__()>1): 
+        #guardando clave valor
         r.set(data[0],data[1])
+        #guardando set para ordenamiento
         r.zadd("OrderSet",{data[0]:int(data[1])})
+#cerrando archivo
 file1.close() 
 
+#Consultas simples
 consult=["ARIEL", "Bastard", "extra-ordinary","Caesar's", "Menelaus", "Achilles!", "Warwick", "WORKS--WILLIAM", "destroy'd?", "epitaph!"]
 print("\n1) Numeros de frecunecia:\n")
 for c in consult:
     data=r.get(c)
     print(c+': '+data.decode())
+
+#consultas ordenadas
 print("2) Primeras 10 palabras ordenadas por frecuencia:\n")
 for c in r.zrange("OrderSet",0,10,desc=True):
     print(c.decode())
+
+#consultas por rango
 print("3)1. Palabras con 100 ocurrencias exactas:\n")
 for c in r.zrangebyscore("OrderSet",100,100):
     print(c.decode())
